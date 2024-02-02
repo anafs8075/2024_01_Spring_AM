08dc1a9d85b3210dc2d8e3a9c69cf667d38e1718
anafs8075 <anafs8075@gmail.com>
2024-02-02 15:22:52 +0900

    NeedLoginInterceptor가 등록된 액션이 실행되기 전에 로그인 체크를 미리 수행

--- src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/example/demo/interceptor/NeedLoginInterceptor.java
@@ -3,19 +3,29 @@ package com.example.demo.interceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		Rq rq = new Rq(req, resp);
+		Rq rq = (Rq) req.getAttribute("rq");
 
-		req.setAttribute("rq", rq);
+		if (!rq.isLogined()) {
+			System.out.println("==============로그인 하고 써============");
+//			resp.getWriter().append("<script>~~~")
+
+			rq.printHistoryBack("로그인 하고 써라");
+
+			return false;
+
+		}
+
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 }
