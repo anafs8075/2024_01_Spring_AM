8969468b1b81cc81ce13638650c14258c899d9d7
anafs8075 <anafs8075@gmail.com>
2024-02-06 16:36:59 +0900

    pagination 적용, 파라미터에서 page 를 가져와 active 활성화

--- DB.sql
+++ DB.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS `Spring_AM_01`;
 CREATE DATABASE `Spring_AM_01`;
 USE `Spring_AM_01`;
+
 # article 테이블 생성
 CREATE TABLE article(
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -9,6 +10,7 @@ CREATE TABLE article(
     title CHAR(100) NOT NULL,
     `body` TEXT NOT NULL
 );
+
 # member 테이블 생성
 CREATE TABLE `member`(
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -24,22 +26,33 @@ CREATE TABLE `member`(
     delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
     delDate DATETIME COMMENT '탈퇴 날짜'
 );
+
+
 # article TD 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목1',
 `body` = '내용1';
+
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목2',
 `body` = '내용2';
+
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4';
+
 # member TD 생성
 # (관리자)
 INSERT INTO `member`
@@ -52,6 +65,7 @@ loginPw = 'admin',
 nickname = '관리자',
 cellphoneNum = '01012341234',
 email = 'abcd@gmail.com';
+
 # (일반)
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -62,6 +76,7 @@ loginPw = 'test1',
 nickname = '회원1',
 cellphoneNum = '01043214321',
 email = 'abcde@gmail.com';
+
 # (일반)
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -81,14 +96,77 @@ WHERE id IN (1,2);
 
 UPDATE article
 SET memberId = 3
+WHERE id IN (3,4);
+
+
+# board 테이블 생성
+CREATE TABLE board(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항), free(자유), QnA(질의응답) ...',
+    `name` CHAR(20) NOT NULL UNIQUE COMMENT '게시판 이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제 여부 (0=삭제 전, 1=삭제 후)',
+    delDate DATETIME COMMENT '삭제 날짜'
+);
+
+# board TD 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'NOTICE',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'FREE',
+`name` = '자유';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'QnA',
+`name` = '질의응답';
+
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+
+UPDATE article
+SET boardId = 1
+WHERE id IN (1,2);
+
+UPDATE article
+SET boardId = 2
 WHERE id = 3;
+
+UPDATE article
+SET boardId = 3
+WHERE id = 4;
 
 ###############################################
 
+INSERT INTO article
+(
+    regDate, updateDate, memberId, boardId, title, `body`
+)
+SELECT NOW(),NOW(), FLOOR(RAND() * 2) + 2, FLOOR(RAND() * 3) + 1, CONCAT('제목_',RAND()), CONCAT('내용_',RAND())
+FROM article;
+
+SELECT FLOOR(RAND() * 2) + 2
+
+SELECT FLOOR(RAND() * 3) + 1
+
+
 SHOW FULL COLUMNS FROM `member`;
 DESC `member`;
+
 SELECT *
 FROM article;
+
 SELECT *
 FROM `member`;
+
+SELECT *
+FROM `board`;
+
 SELECT LAST_INSERT_ID();
